f0ae4e9c5b196ee908d5c06ef53792600b17bde1
Fiend3333 <116087739+Fiend3333@users.noreply.github.com>
2023-05-26 02:06:48 +0000

    ajuste

--- utils.h
+++ utils.h
@@ -1,138 +1,134 @@
-#ifndef __UTILS__
-#define __UTILS__
+#ifndef UTILS_H
+#define UTILS_H
 
-
-typedef struct{
+typedef struct {
   char nome[60];
   int id;
-  char* email;
-  char* senha;
-  char* tipoDeUsuario;
-  void* categoriaUsuario;
+  char *email;
+  char *senha;
+  char *tipoDeUsuario;
+  void *categoriaUsuario;
   int status;
-  
-}Usuario;
 
+} Usuario;
 
 // Tipos de Usuarios
 
-typedef struct{
+typedef struct {
   Usuario usuario;
   int matricula;
   Turma turma;
-  Atividade* listaAtividades;
-  Submissao* listaSubmissoes;
+  Atividade *listaAtividades;
+  Submissao *listaSubmissoes;
   Preceptor preceptorResponsavel;
   float notasTrimestrais[];
-  FeedbackGeral* listaFeedbacks;
-}Residente;
+  FeedbackGeral *listaFeedbacks;
+} Residente;
 
-typedef struct{
+typedef struct {
   Usuario usuario;
-  Residente* listaResidentesAtuais;
-  Atividade* listaAtividades;
-  ProgramaResidencia* listaResidencias;
-  FeedbackGeral* listaFeedbacks;
-}Preceptor;
+  Residente *listaResidentesAtuais;
+  Atividade *listaAtividades;
+  ProgramaResidencia *listaResidencias;
+  FeedbackGeral *listaFeedbacks;
+} Preceptor;
 
-typedef struct{
-  Usuario usuario;
-  char cargo[];
-  ProgramaResidencia residencia; residencia;
-}Coordenacao;
-
-typedef struct{
+typedef struct {
   Usuario usuario;
   char cargo[];
-  ProgramaResidencia* listaResidencias;
-}Gestao;
+  ProgramaResidencia residencia;
+  residencia;
+} Coordenacao;
 
+typedef struct {
+  Usuario usuario;
+  char cargo[];
+  ProgramaResidencia *listaResidencias;
+} Gestao;
 
 // Conjuntos
 
-typedef struct{
+typedef struct {
   char nomeResidencia[];
   char anoDaTurma[];
-  Residente* residentes;
-  Preceptor* listaPreceptores;
-  Atividade* listasAtividades;
+  Residente *residentes;
+  Preceptor *listaPreceptores;
+  Atividade *listasAtividades;
   ProgramaResidencia residencia;
-  char* criteriosAvaliativos;
-}Turma;
+  char *criteriosAvaliativos;
+} Turma;
 
-typedef struct{
+typedef struct {
   char nomePrograma[];
-  Coordenacao* listaCoordenacao;
-  Turma* listaTurmas
-}ProgramaResidencia;
-
+  Coordenacao *listaCoordenacao;
+  Turma *listaTurmas
+} ProgramaResidencia;
 
 // Funcionalidades da solução
 
-typedef struct{
+typedef struct {
   char nomeDaAtividade[];
   char descricaoDaAtividade[];
   Turma turma;
   char programaDeResidencia[];
-  Submissao* listaSubmissao;
+  Submissao *listaSubmissao;
   char dataDaPostagem;
   char dataDaEntrega;
   int ativa;
-}Atividade;
+} Atividade;
 
-typedef struct{
+typedef struct {
   Residente residente;
   Preceptor preceptor;
   Atividade atividade;
   float nota;
-  char * resposta;
+  char *resposta;
   char feedback[];
-  char* status;
-}Submissao;
+  char *status;
+} Submissao;
 
-typedef struct{
+typedef struct {
   Preceptor preceptor;
   char *criterios;
   float nota;
   char *feedback;
-  char* contestacao
-  char* respostaConstentacao;
+  char *contestacao char *respostaConstentacao;
   char remetente;
   char destinatário;
   char data[];
   char status(para constentacao);
-}FeedbackGeral;
+} FeedbackGeral;
 
 // Funções
 
 void fazerCadastro();
 
-Usuario* fazerLogin(char* email, char* senha,  Usuario *usuario);
+Usuario *fazerLogin(char *email, char *senha, Usuario *usuario);
 
-void criarCriteriosDeFeedback(Turma* turma);
+void criarCriteriosDeFeedback(Turma *turma);
 
-void criarAtividades(Turma* turma);
+void criarAtividades(Turma *turma);
 
-void visualizarTurmas(Turma* listaTurmas );
+void visualizarTurmas(Turma *listaTurmas);
 
-void visualizarFeedbacks(char* nome);
+void visualizarFeedbacks(char *nome);
 
-void visualizarAtividades( Atividade* listaAtividades);
+void visualizarAtividades(Atividade *listaAtividades);
 
-void submeterAtividade( Residente* residente, Submissao* submissao);
+void submeterAtividade(Residente *residente, Submissao *submissao);
 
-void visualizarSubmissao(Submissao* submissao);
+void visualizarSubmissao(Submissao *submissao);
 
-void contestarFeedback(Usuario* usuario, feedbackGeral feedbackGeral);
+void contestarFeedback(Usuario *usuario, feedbackGeral feedbackGeral);
 
-void visualizarUsuario(Usuario* usuario);
+void visualizarUsuario(Usuario *usuario);
 
-void avaliarAtividade(Preceptor* preceptor, Submissao* submissao);
+void avaliarAtividade(Preceptor *preceptor, Submissao *submissao);
 
-void fazerAvaliacaoTrimestral( Usuario* usuario, float notaTrimestral);
+void fazerAvaliacaoTrimestral(Usuario *usuario, float notaTrimestral);
 
-void fazerFeedback(Preceptor* preceptor, Residente* residente, Usuario usuario);
+void fazerFeedback(Preceptor *preceptor, Residente *residente, Usuario usuario);
 
-void cadastrarTurma(ProgramaResidencia* residencia);
+void cadastrarTurma(ProgramaResidencia *residencia);
 
-#endif //__UTILS__
+#endif
